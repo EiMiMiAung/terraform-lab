80905b5f6ac9221996c5d650c1e04fd8579bfd36
mimi <user@usernoMacBook-Pro.local>
2025-02-18 23:32:47 +0900

    update provider

--- provider.tf
+++ provider.tf
@@ -4,13 +4,8 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.84.0"
     }
-    #    azurerm = {
-    #     source = "hashicorp/azurerm"
-    #     version = "4.16.0"
-    #   }
-    #   kubernetes = {
-    #     source = "hashicorp/kubernetes"
-    #     version = "2.35.1"
-    #   }
   }
+}
+provider "aws" {
+
 }
