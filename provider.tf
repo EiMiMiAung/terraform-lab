a4c86d93d3058b93a9ac6df876f8f1ac0874cad1
mimi <user@usernoMacBook-Pro.local>
2025-02-18 23:31:38 +0900

    add provider

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+    #    azurerm = {
+    #     source = "hashicorp/azurerm"
+    #     version = "4.16.0"
+    #   }
+    #   kubernetes = {
+    #     source = "hashicorp/kubernetes"
+    #     version = "2.35.1"
+    #   }
+  }
+}
